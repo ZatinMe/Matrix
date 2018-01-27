8453009b3b18c596a0d7979a61d0f52ede17b3ec
ZatinMe <meraz.zatin@gmail.com>
2018-01-27 10:30:21 +0530

    guys login and register is addedbut not functional yet in the alumini page

--- matrix/alumini/urls.py
+++ matrix/alumini/urls.py
@@ -5,4 +5,6 @@ app_name = 'alumini'
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
+	url(r'^login_al', views.index1, name='index1'),
+	url(r'^login_re', views.index2, name='index2'),
 ]
--- matrix/alumini/views.py
+++ matrix/alumini/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def index(request):
 	return render(request, 'alumini/index.html')
+
+def index1(request):
+	return render(request, 'alumini/index1.html')	
+
+def index2(request):
+	return render(request, 'alumini/index2.html')		
